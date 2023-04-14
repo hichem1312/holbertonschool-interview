f28aa49e2c7d172933a0a217db0f2c93e0c8634b
hichem <hichem@hichem.com>
2023-04-14 02:32:09 +0100

    method that calculates the fewest number of operations needed

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""  a method that calculates the fewest number of operations needed 
+to result in exactly n H characters in the file. """
+
+def minOperations(n):
+
+    a = 2
+    b = 0
+    if (n < 2):
+        return 0
+    test = n
+    while (test != 1):
+        while (test % a == 0):
+            test = test / a
+            b += a
+        a += 1
+    return b
