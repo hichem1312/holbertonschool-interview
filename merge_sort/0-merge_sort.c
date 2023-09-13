b81a2c3923c5ed09b9843a1390f877b90bca09af
hichem <hichem@hichem.com>
2023-09-13 03:03:19 +0100

    function that sorts an array of integers in ascending order using the Merge Sort

---
+++ merge_sort/0-merge_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * merge_sort - sort an array of integers in ascending order using Merge Sort
+ * @array: array to sort
+ * @size: size of the array
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *tmp = malloc(size * sizeof(int));
+    size_t left_side = size / 2, right_side = size - left_side;
+	size_t v = 0, i = 0, j = 0;
+	int *tmp_array = array + left_side;
+
+	if (tmp == NULL)
+		return;
+
+	if (size <= 1)
+		return;
+
+	merge_sort(array, left_side);
+	merge_sort(tmp_array, right_side);
+
+	for (i = 0; i < left_side; i++)
+		tmp[i] = array[i];
+
+	printf("Merging...\n[left]: ");
+	print_array(array, left_side);
+	printf("[right]: ");
+	print_array(tmp_array, right_side);
+
+	i = 0;
+
+	while (i < left_side || j < right_side)
+	{
+		while (j < right_side && (i >= left_side || tmp_array[j] < tmp[i]))
+			array[v++] = tmp_array[j++];
+		while (i < left_side && (j >= right_side || tmp_array[j] >= tmp[i]))
+			array[v++] = tmp[i++];
+	}
+
+	printf("[Done]: ");
+	print_array(array, size);
+    free(tmp);
+}
