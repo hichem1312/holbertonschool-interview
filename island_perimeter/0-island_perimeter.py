4c4b4905727936174c44e7598a78ba8123fcc3a6
hichem <hichem@hichem.com>
2023-10-20 01:38:03 +0100

    .

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -5,20 +5,20 @@ a function that returns the perimeter of the island described
 
 
 def island_perimeter(grid):
-"""island perimeter function"""
-if not grid:
-	return 0
+	"""island perimeter function"""
+	if not grid:
+		return 0
 
-x = len(grid)
-y = len(grid[0])
-p = 0
+	x = len(grid)
+	y = len(grid[0])
+	p = 0
 
-for i in range(x):
-	for j in range(y):
-		if grid[i][j] == 1:
-			p += 4
-			if i > 0 and grid[i-1][j] == 1:
-				p -= 2
-			if j > 0 and grid[i][j-1] == 1:
-				p -= 2
-return p
+	for i in range(x):
+		for j in range(y):
+			if grid[i][j] == 1:
+				p += 4
+				if i > 0 and grid[i-1][j] == 1:
+					p -= 2
+				if j > 0 and grid[i][j-1] == 1:
+					p -= 2
+	return p
