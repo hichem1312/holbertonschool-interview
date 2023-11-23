2501a1e4f13a36e6fdbc6c4f9f1e024736c5faa7
hichem <hichem@hichem.com>
2023-11-23 04:17:51 +0100

    a script that prints all characters of a Star Wars movie

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,8 +4,8 @@
 const args = process.argv.slice(2);
 if (args < 1)
 {
-  console.log('Error');
-  process.exit(1);
+	console.log('Error');
+	process.exit(1);
 }
 
 const episode = args[0];
@@ -13,30 +13,28 @@ const episode = args[0];
 const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const request = require('request');
 let characters = [];
-request(url, (err, resp, body) =>
-{
-  if (err || resp.statusCode !== 200) console.log(err);
-  else characters = JSON.parse(body).characters;
-  const size = Object.keys(characters).length;
-  const array = Array(size).fill();
-  let data = 0;
-  for (let i = 0; i < size; i++)
-  {
-	request(characters[i], (erro, respo, bodys) =>
+request(url, (err, resp, body) => {
+	if (err || resp.statusCode !== 200) console.log(err);
+	else characters = JSON.parse(body).characters;
+	const size = Object.keys(characters).length;
+	const array = Array(size).fill();
+	let data = 0;
+	for (let i = 0; i < size; i++)
 	{
-	  if (erro || respo.statusCode !== 200) console.log(erro);
-	  else
-	  {
-		array[i] = JSON.parse(bodys).name;
-		data++;
-	  }
-	  if (data === size) {
+	request(characters[i], (erro, respo, bodys) => {
+		if (erro || respo.statusCode !== 200) console.log(erro);
+		else
+		{
+			array[i] = JSON.parse(bodys).name;
+			data++;
+		}
+		if (data === size) {
 		for (let j = 0; j < size; j++)
 		{
-		  console.log(array[j]);
+			console.log(array[j]);
 		}
-	  }
+		}
 	}
 	);
-  }
+	}
 });
