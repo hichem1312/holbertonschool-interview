4ae2b57451f56fa183df9dffc5407918c793fcc8
hichem <hichem@hichem.com>
2023-12-07 00:38:07 +0100

    calculate how many square units of water will be retained after it rains

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,24 +3,24 @@
 
 
 def rain(walls):
-"""function"""
-n = len(walls)
-if n == 0:
-	return 0
+	"""function"""
+	n = len(walls)
+	if n == 0:
+		return 0
 
-l_max = [0] * n
-r_max = [0] * n
+	l_max = [0] * n
+	r_max = [0] * n
 
-l_max[0] = walls[0]
-for i in range(1, n):
-	l_max[i] = max(l_max[i-1], walls[i])
+	l_max[0] = walls[0]
+	for i in range(1, n):
+		l_max[i] = max(l_max[i-1], walls[i])
 
-r_max[n-1] = walls[n-1]
-for i in range(n-2, -1, -1):
-	r_max[i] = max(r_max[i+1], walls[i])
+	r_max[n-1] = walls[n-1]
+	for i in range(n-2, -1, -1):
+		r_max[i] = max(r_max[i+1], walls[i])
 
-x = 0
-for i in range(n):
-	x += min(l_max[i], r_max[i]) - walls[i]
+	x = 0
+	for i in range(n):
+		x += min(l_max[i], r_max[i]) - walls[i]
 
-return x if x >= 0 else 0
+	return x if x >= 0 else 0
